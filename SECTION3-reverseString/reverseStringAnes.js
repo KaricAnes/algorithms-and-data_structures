547bd94c3fbd0ccd3395abcd2e0a8245899b4564
KaricAnes <anes.karic@stu.ibu.edu.ba>
2020-12-12 19:12:32 +0100

    ReverseString three solutions

---
+++ SECTION3-reverseString/reverseStringAnes.js
@@ -0,0 +1,12 @@
+function reverse(str) {
+  let reversedString = "";
+  let strLength = str.length; //4
+  for (let i = strLength - 1; i >= 0; i--) {
+    //str[4] je undefined; str [3] je d. zato sam stavio strLength - 1
+    debugger;
+    reversedString = reversedString + str[i]; //sto je nekad bio, plus novo. Dok nije bilo ovo, samo mi je zadnje slovo vracao. Tj. a
+  }
+  //reversedString = str[3] + str[2] + str[1] + str[0]; //manuelno rijesen zadatak
+  return reversedString;
+}
+reverse("abcd");
