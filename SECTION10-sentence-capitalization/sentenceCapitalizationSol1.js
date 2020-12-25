355a0d03ecf67930353d6141aee36472e538218b
KaricAnes <anes.karic@stu.ibu.edu.ba>
2020-12-25 17:22:16 +0100

    sentence capitalization solved with split-join, and slice

---
+++ SECTION10-sentence-capitalization/sentenceCapitalizationSol1.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+//slice
+/*
+const word = "there";
+console.log(word.slice(2)); //ere
+*/
+
+const capitalize = (str) => {
+  const words = [];
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(" ");
+};
+
+console.log(capitalize("aki short sentence ddffd"));
+console.log(capitalize("ja sam anes"));
